dec2b02293eba75b096ad80d12fd0c952752e084
NOEL-code <junusong12@hanyang.ac.kr>
2024-04-08 14:47:31 +0900

    로그인 구현

--- JavaPratice/untitled/src/day03/EX06.java
+++ JavaPratice/untitled/src/day03/EX06.java
@@ -1,25 +1,37 @@
 package day03;
 
+import java.util.LinkedList;
+
 public class EX06 {
     public static void main(String[] args) {
 
-
-        // Person => ApplePhone 모든 기능
-        // 30분까지
-        // ApplePhone => SamsungPhone 기기변경
     }
 }
 
-class ApplePhone {
+interface Phone {
+    int BATTERY_MAX_CAPA = 100; // 배터리 최대 용량
+    int batteryStatus = 70; // 배터리 현재 용량
+    boolean isOn = true;
+
+    boolean getIsOn();
+    void charge();
+    void operateKakaotalk();
+    void printBatteryStatus();
+    void turnOff();
+    void  turnOn();
+    void useYoutube();
+}
+
+class SamsungPhone implements Phone {
     private final int BATTERY_MAX_CAPA = 100; // 배터리 최대 용량
     private int batteryStatus = 70; // 배터리 현재 용량
     private boolean isOn = true;
 
-    boolean getIsOn() {
+    public boolean getIsOn() {
         return isOn;
     }
 
-    void charge() {
+    public void charge() {
         if (batteryStatus == BATTERY_MAX_CAPA) {
             System.out.println("100%입니다.");
             return;
@@ -33,7 +45,7 @@ class ApplePhone {
                 + "%가 되었습니다.");
     }
 
-    void operateKakaotalk() {
+    public void operateKakaotalk() {
         if (isOn) {
             batteryStatus -= 5;
 
@@ -42,31 +54,116 @@ class ApplePhone {
         }
     }
 
-    void checkBatteryStatus() {
+    public void printBatteryStatus() {
         System.out.println(batteryStatus+"%");
     }
 
-    // useYoutube() -10
-    // turnOn
-    // turnOff
+    public void turnOff(){
+        isOn = false;
+    }
+    public void  turnOn(){
+        isOn = true;
+    }
+    public void useYoutube(){
+        if (isOn) {
+            batteryStatus -= 10;
+
+            if (batteryStatus == 0)
+                turnOff();
+        }
+    }
+}
+
+class ApplePhone implements Phone{
+    private final int BATTERY_MAX_CAPA = 100; // 배터리 최대 용량
+    private int batteryStatus = 70; // 배터리 현재 용량
+    private boolean isOn = true;
+
+    public boolean getIsOn() {
+        return isOn;
+    }
+
+    public void charge() {
+        if (batteryStatus == BATTERY_MAX_CAPA) {
+            System.out.println("100%입니다.");
+            return;
+        } else if (batteryStatus == 95) {
+            batteryStatus += 5;
+        } else {
+            batteryStatus += 10;
+        }
+
+        System.out.println(batteryStatus
+                + "%가 되었습니다.");
+    }
+
+    public void operateKakaotalk() {
+        if (isOn) {
+            batteryStatus -= 5;
+
+            if (batteryStatus == 0)
+                turnOff();
+        }
+    }
+
+    public void printBatteryStatus() {
+        System.out.println(batteryStatus+"%");
+    }
+
+    public void turnOff(){
+        isOn = false;
+    }
+    public void  turnOn(){
+        isOn = true;
+    }
+    public void useYoutube(){
+        if (isOn) {
+            batteryStatus -= 10;
+
+            if (batteryStatus == 0)
+                turnOff();
+        }
+    }
 }
 
 class Person {
-    ApplePhone phone;
 
-    // buyPhone()
-    void buyPhone(ApplePhone phone) {
+    Phone phone; // Now it can hold either ApplePhone or SamsungPhone
+
+    void buyPhone(Phone phone) {
         this.phone = phone;
     }
 
-    // charge()
-    void chargePhone() {
-        phone.charge();
+    void changePhone(Phone phone) {
+        this.phone = phone;
     }
 
     // useKakaotalk()
     void useKakaotalk() {
-        if (phone.getIsOn())
-            phone.operateKakaotalk();
+        if (this.phone.getIsOn())
+            this.phone.operateKakaotalk();
+    }
+    void useYouTube() {
+        if (this.phone.getIsOn())
+            this.phone.useYoutube();
+    }
+
+    void checkBatteryStatus() {
+        if (this.phone.getIsOn())
+            this.phone.printBatteryStatus();
+    }
+
+    void turnOn() {
+        if(!this.phone.getIsOn()){
+            this.phone.turnOn();
+        }
+    }
+    void charge(){
+        this.phone.charge();
+    }
+    void turnOff(){
+        if(this.phone.getIsOn()){
+            this.phone.turnOff();
+        }
     }
 }
